ecc311515cc5576203f79ca55dc2da03bc88b4a1
mubzie <85705074+mubzie@users.noreply.github.com>
2023-10-08 12:19:29 +0100

    fixed footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import instagram from "/src/assets/instagram.png";
 import fb from "/src/assets/fb.png";
-import yt from "/src/assets/yt.png";
 import twitter from "/src/assets/twitter.png";
-import Button from "../Button/Button";
 
 const Footer = () => {
   return (
     <>
-      <div className={styles.footerWrapperContainer}>
-        <div className={styles.socialMediaContainer}>
+      <div className={styles.wrapperContainer}>
+        <div className={styles.mediaContainer}>
           <img src={fb} className={styles.smIcon}></img>
-          <img src={instagram} className={styles.smIcon}></img>
           <img src={twitter} className={styles.smIcon}></img>
-          <img src={yt} className={styles.smIcon}></img>
         </div>
 
-        <ul className={styles.footerListItems}>
-          <li className="listItems">condition of use</li>
-          <li className="listItems">privacy & policy</li>
-          <li className="listItems">press room</li>
-        </ul>
-
-        <div className={styles.copyRightText}>
-          &#169; 2023 MovieBox by Mubarak Rabiu
-        </div>
+        <div className={styles.copyRightText}>made with love❤️ by mubzie</div>
       </div>
     </>
   );
