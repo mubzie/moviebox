305aef0711fc8b488afcbb01a7d34302e306ff2d
mubzie <85705074+mubzie@users.noreply.github.com>
2023-10-26 08:44:41 +0100

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { qrcode } from "vite-plugin-qrcode";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/moviebox/",
   plugins: [react(), qrcode()],
   build: {},
 });
